5c56c44b8c732fccf30b5eae2375f3c329c1875b
Kenneth Madondo <kenneth.madondo@yahoo.com>
2024-07-25 15:36:11 +0200

    added animated background

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const checkBox = document.querySelector(".checkbox")
 const body = document.body;
 const header =document.querySelector(".header")
 const link = document.querySelectorAll('a')
+const animatedBg = document.querySelector(".background")
 
 menu.addEventListener("click",(e)=>{
 links.classList.toggle("showlinks")
@@ -17,6 +18,7 @@ closeMenu.addEventListener("click",()=>{
 modeSwitch.addEventListener('click',(e)=>{  
     modeSwitch.classList.toggle('dark')
     body.classList.toggle("dark-mode")
+    animatedBg.classList.toggle("showanimated")
     link.forEach((a)=>{
         a.classList.toggle('dark-mode')
     })
@@ -29,6 +31,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     if(theme == 'dark'){
         body.classList.add("dark-mode")
         header.classList.add("dark-mode")
+        animatedBg.classList.add("showanimated")
         link.forEach((a)=>{
             a.classList.add('dark-mode')
         })
